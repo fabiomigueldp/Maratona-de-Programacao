77137faed34888dee4b3e570de6a060f2cb8852b
Fábio Miguel <fabiomigueldp@gmail.com>
2025-05-07 23:33:36 -0300

    fix2

--- 2025-1/Outros/simulacao_de_uno.c
+++ 2025-1/Outros/simulacao_de_uno.c
@@ -2,54 +2,48 @@
 
 int main()
 {
- short cartas1=5,cartas2=5,vez=1,uno1=0,uno2=0,manter=0;
- char jogada;
-
- while(scanf(" %c",&jogada)==1)
+ short c1=5,c2=5,vez=1,uno1=0,uno2=0,manter;
+ char j;
+ while(scanf(" %c",&j)==1)
  {
-  if(jogada=='U')
+  if(j!='U')
+  {
+   if(vez==1&&c1==1&&uno1==0)
+   {
+    printf("ESQUECEU DE FALAR UNO!!\n");
+    c1+=1;
+    vez=2;
+    uno1=0;
+    printf("Jogador 1: %hd cartas\n",c1);
+    printf("Jogador 2: %hd cartas\n",c2);
+    continue;
+   }
+   if(vez==2&&c2==1&&uno2==0)
+   {
+    printf("ESQUECEU DE FALAR UNO!!\n");
+    c2+=1;
+    vez=1;
+    uno2=0;
+    printf("Jogador 1: %hd cartas\n",c1);
+    printf("Jogador 2: %hd cartas\n",c2);
+    continue;
+   }
+  }
+
+  if(j=='U')
   {
    if(vez==1)
    {
-    if(cartas1==1&&uno1==0)
-    {
-     uno1=1;
-     continue;
-    }
-    vez=2;
+    if(c1==1&&uno1==0){uno1=1;continue;}
+    vez=2;uno1=0;
    }
    else
    {
-    if(cartas2==1&&uno2==0)
-    {
-     uno2=1;
-     continue;
-    }
-    vez=1;
+    if(c2==1&&uno2==0){uno2=1;continue;}
+    vez=1;uno2=0;
    }
-
-   printf("Jogador 1: %hd cartas\n",cartas1);
-   printf("Jogador 2: %hd cartas\n",cartas2);
-   continue;
-  }
-
-  if(vez==1&&cartas1==1&&uno1==0)
-  {
-   printf("ESQUECEU DE FALAR UNO!!\n");
-   cartas1+=1;
-   vez=2;
-   printf("Jogador 1: %hd cartas\n",cartas1);
-   printf("Jogador 2: %hd cartas\n",cartas2);
-   continue;
-  }
-
-  if(vez==2&&cartas2==1&&uno2==0)
-  {
-   printf("ESQUECEU DE FALAR UNO!!\n");
-   cartas2+=1;
-   vez=1;
-   printf("Jogador 1: %hd cartas\n",cartas1);
-   printf("Jogador 2: %hd cartas\n",cartas2);
+   printf("Jogador 1: %hd cartas\n",c1);
+   printf("Jogador 2: %hd cartas\n",c2);
    continue;
   }
 
@@ -57,62 +51,39 @@ int main()
 
   if(vez==1)
   {
-   cartas1-=1;
-
-   if(jogada=='2')
-   {
-    if(cartas1) cartas2+=2;
-    manter=1;
-   }
-   else if(jogada=='4')
-   {
-    if(cartas1) cartas2+=4;
-    manter=1;
-   }
-   else if(jogada=='R'||jogada=='B')
-    manter=1;
-
-   if(cartas1==0)
+   c1-=1;
+   if(j=='2'){if(c1)c2+=2;manter=1;}
+   else if(j=='4'){if(c1)c2+=4;manter=1;}
+   else if(j=='R'||j=='B')manter=1;
+   if(c1==0)
    {
     printf("Jogador 1 VENCEU!!\n");
-    printf("Jogador 2 tinha %hd cartas!\n",cartas2);
+    printf("Jogador 2 tinha %hd cartas!\n",c2);
     return 0;
    }
-
-   if(cartas1!=1) uno1=0;
   }
   else
   {
-   cartas2-=1;
-
-   if(jogada=='2')
-   {
-    if(cartas2) cartas1+=2;
-    manter=1;
-   }
-   else if(jogada=='4')
-   {
-    if(cartas2) cartas1+=4;
-    manter=1;
-   }
-   else if(jogada=='R'||jogada=='B')
-    manter=1;
-
-   if(cartas2==0)
+   c2-=1;
+   if(j=='2'){if(c2)c1+=2;manter=1;}
+   else if(j=='4'){if(c2)c1+=4;manter=1;}
+   else if(j=='R'||j=='B')manter=1;
+   if(c2==0)
    {
     printf("Jogador 2 VENCEU!!\n");
-    printf("Jogador 1 tinha %hd cartas!\n",cartas1);
+    printf("Jogador 1 tinha %hd cartas!\n",c1);
     return 0;
    }
-
-   if(cartas2!=1) uno2=0;
   }
 
-  printf("Jogador 1: %hd cartas\n",cartas1);
-  printf("Jogador 2: %hd cartas\n",cartas2);
+  printf("Jogador 1: %hd cartas\n",c1);
+  printf("Jogador 2: %hd cartas\n",c2);
 
-  if(!manter) vez=(vez==1)?2:1;
+  if(!manter)
+  {
+   if(vez==1){vez=2;uno1=0;}
+   else      {vez=1;uno2=0;}
+  }
  }
-
  return 0;
 }
